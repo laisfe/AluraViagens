fd1f529b2acdadc9a5dd872efc93426e4cf48198
Lais Fialho <laisffn@hotmail.com>
2023-01-09 13:26:48 -0300

    Exibe header na view

---
+++ AluraViagens/HomeTableViewHeader.swift
@@ -0,0 +1,20 @@
+//
+//  HomeTableViewHeader.swift
+//  AluraViagens
+//
+//  Created by Lais on 09/01/23.
+//
+
+import UIKit
+
+class HomeTableViewHeader: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
