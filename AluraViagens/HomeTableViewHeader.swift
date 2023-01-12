607fc8363a32314e6b9a01d8a8165bd1c0fed6bd
Lais Fialho <laisffn@hotmail.com>
2023-01-12 01:06:56 -0300

    Adiciona constraints

--- AluraViagens/HomeTableViewHeader.swift
+++ AluraViagens/HomeTableViewHeader.swift
@@ -14,4 +14,8 @@ class HomeTableViewHeader: UIView {
     @IBOutlet weak var tituloLabel: UILabel!
     @IBOutlet weak var headerView: UIView!
     @IBOutlet weak var bannerImageView: UIImageView!
+    
+    func configuraView() {
+        headerView.backgroundColor = UIColor(red: 30.0/255.0, green: 59.0/255.0, blue: 119.0/255.0, alpha: 1)
+    }
 }
