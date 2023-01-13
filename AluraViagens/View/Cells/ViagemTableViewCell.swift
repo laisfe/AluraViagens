5aae7c22303220d0913b8b99a04bdf1cd62de388
Lais Fialho <laisffn@hotmail.com>
2023-01-13 17:41:43 -0300

    Cria nova celula

---
+++ AluraViagens/View/Cells/ViagemTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ViagemTableViewCell.swift
+//  AluraViagens
+//
+//  Created by Lais on 13/01/23.
+//
+
+import UIKit
+
+class ViagemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
