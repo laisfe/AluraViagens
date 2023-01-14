99e60f14d8ffaec8ba0072644fd2e5019ab95a8f
Lais Fialho <laisffn@hotmail.com>
2023-01-13 21:17:53 -0300

    Configura outlets da TableView

--- AluraViagens/View/Cells/ViagemTableViewCell.swift
+++ AluraViagens/View/Cells/ViagemTableViewCell.swift
@@ -19,5 +19,22 @@ class ViagemTableViewCell: UITableViewCell {
     @IBOutlet weak var precoViagemLabel: UILabel!
     @IBOutlet weak var statusCancelamentoViagemLabel: UILabel!
     
-    
+    func configuraCelula(_ viagem: Viagem?) {
+        viagemImage.image = UIImage(named: viagem?.asset ?? "")
+        tituloViagemLabel.text = viagem?.titulo
+        subtituloViagemLabel.text = viagem?.subtitulo
+        precoViagemLabel.text = "R$ \(viagem?.preco ?? 0)"
+        
+        //Cria risco em cima de label
+        let atributoString: NSMutableAttributedString = NSMutableAttributedString(string: "R$ \(viagem?.precoSemDesconto ?? 0)")
+        atributoString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: 1, range: NSMakeRange(0, atributoString.length))
+        precoSemDescontoLabel.attributedText = atributoString
+        
+        if let numeroDias = viagem?.diaria, let nummeroHospedes = viagem?.hospedes {
+            let diarias = numeroDias == 1 ? "Diaria" : "Diarias"
+            let hospedes = nummeroHospedes == 1 ? "Pessoa" : "Pessoas"
+            
+            diariaViagemLabel.text = "\(numeroDias) \(diarias) - \(nummeroHospedes) \(hospedes)"
+        }
+    }
 }
