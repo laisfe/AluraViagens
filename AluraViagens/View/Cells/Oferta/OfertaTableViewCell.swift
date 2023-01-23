d738b10eed196e63d0de04605e61b91f2045f0c8
Lais Fialho <laisffn@hotmail.com>
2023-01-23 14:39:19 -0300

    Cria outlets múltiplos

--- AluraViagens/View/Cells/Oferta/OfertaTableViewCell.swift
+++ AluraViagens/View/Cells/Oferta/OfertaTableViewCell.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class OfertaTableViewCell: UITableViewCell {
 
+    //MARK: - IBOutlets
+    
+    @IBOutlet var viagemImages: [UIImageView]!
+    @IBOutlet var tituloViagemLabels: [UILabel]!
+    @IBOutlet var subtituloViagemLabels: [UILabel]!
+    @IBOutlet var precoSemDescontonLabels: [UILabel]!
+    @IBOutlet var precoLabels: [UILabel]!
+    
+    
     
     
 }
