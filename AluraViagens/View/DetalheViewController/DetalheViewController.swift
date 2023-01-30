a31aa953a7813dc746f2ec8df3faf2d3a59f0873
Lais Fialho <laisffn@hotmail.com>
2023-01-30 16:26:52 -0300

    Cria layout tela de detalhes

--- AluraViagens/View/DetalheViewController/DetalheViewController.swift
+++ AluraViagens/View/DetalheViewController/DetalheViewController.swift
@@ -9,10 +9,21 @@ import UIKit
 
 class DetalheViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var viagemImage: UIImageView!
+    @IBOutlet weak var tituloViagemmLabel: UILabel!
+    @IBOutlet weak var subtituloViagemLabel: UILabel!
+    
+    // MARK: - View life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
+    
+    // MARK: - Actions
 
+    @IBAction func botaoVoltar(_ sender: UIButton) {
+        navigationController?.popViewController(animated: true)
+    }
 }
